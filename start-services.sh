b05e5a02823e8382fe0fefaf26c89109d75a4918
贝克街的捉虫师 <forpeng@foxmail.com>
2025-05-08 16:50:47 +0800

    Add files via upload

--- start-services.sh
+++ start-services.sh
@@ -4,6 +4,9 @@ x11vnc -storepasswd $VNC_PASSWORD /home/${USERNAME}/.vnc/passwd
 Xvfb :99 -screen 0 1920x1080x24 -ac 2>/dev/null &
 sleep 1
 export DISPLAY=:99
+# 启动 xfce4 桌面环境
+startxfce4 &
+sleep 2
 x11vnc -display :99 -forever -shared -rfbauth /home/${USERNAME}/.vnc/passwd -rfbport 5900 -noxdamage -ncache 10 -ncache_cr 2>/dev/null &
 sleep 1
 /opt/novnc/utils/novnc_proxy --vnc localhost:5900 --listen 6080 2>/dev/null &
